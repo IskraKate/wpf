f3a476696d5841980bd66b3bdb9283c77d517e02
Kate Iskra <kateiskra99@gmail.com>
2020-08-16 21:57:27 +0300

    calc, pipes

--- Calc/Calc/MainWindow.xaml.cs
+++ Calc/Calc/MainWindow.xaml.cs
@@ -20,9 +20,236 @@ namespace Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double? _val1;
+        private double? _val2;
+        private char _sign;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            if (panel.Content.ToString().Length == 1 && panel.Content.ToString() == "0")
+            {
+                _val1 = Convert.ToDouble(button.Content);
+                panel.Content = _val1;
+            }
+            else
+            {
+                panel.Content = panel.Content.ToString() + button.Content.ToString();
+                _val2 = null;
+            }
+
+            //if (panel.Content.ToString().Length == 1 && panel.Content.ToString() == "0")
+            //{
+            //    val1 = Convert.ToDouble(button.Content);
+            //    panel.Content = val1;
+            //}
+            ////else if (panel.Content.ToString().Contains(','))
+            ////{
+            ////    var allAfterDot = panel.Content.ToString().Split(',');
+            ////    var pow = allAfterDot[1].Length;
+            ////    var newAfterDot = Convert.ToDouble(button.Content) * Math.Pow(0.1, pow);
+            ////    val1 += newAfterDot;
+            ////    panel.Content = val1;
+            ////}
+            //else if (panel.Content.ToString().Contains('X'))
+            //{
+            //    if(button.Content.ToString() != "X")
+            //    {
+            //        val2 = Convert.ToDouble(val2.ToString() + button.Content.ToString());
+            //        panel.Content = val1.ToString() +'X'+ val2.ToString();
+            //        var index = panel.Content.ToString().IndexOf('X');
+            //        var str = panel.Content.ToString().Substring(index+1, 1);
+            //    }
+            //}
+            //else
+            //{
+            //    val1 = Convert.ToDouble(val1.ToString() + button.Content.ToString());
+            //    panel.Content = val1;
+            //}
+        }
+
+        private void dot_Click(object sender, RoutedEventArgs e)
+        {
+            if (!panel.Content.ToString().Contains(','))
+            {
+                panel.Content = panel.Content.ToString() + ',';
+            }
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            _val1 = 0;
+            _val2 = null;
+            panel.Content = _val1;
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            var str = panel.Content.ToString();
+
+            if (str.Length > 1)
+            {
+                if(str[str.Length - 2] != ' ')
+                {
+                    str = str.Remove(str.Length - 1, 1);
+                }
+                else
+                {
+                    str = str.Remove(str.Length - 5, 5);
+                }
+
+                panel.Content = str;
+            }
+            else if (str.Length==1)
+            {
+                _val1 = 0;
+                panel.Content = "0";
+            }
+
+        }
+
+        private void multMin_Click(object sender, RoutedEventArgs e)
+        {
+            var expressionArray = panel.Content.ToString().Split(' ');
+            if (expressionArray.Length == 0)
+            {
+                return;
+            }
+
+            double num1 = Convert.ToDouble(expressionArray[0]);
+
+            num1 *= -1;
+            panel.Content= num1.ToString();
+        }
+
+        private void power_Click(object sender, RoutedEventArgs e)
+        {
+            var val = Convert.ToDouble(panel.Content);
+            _val1 = Math.Pow(val, 2);
+            panel.Content = _val1;
+        }
+
+        private void mPower_Click(object sender, RoutedEventArgs e)
+        {
+            var val = Convert.ToDouble(panel.Content);
+            _val1 = 1 / val;
+            panel.Content = _val1;
+        }
+
+        private void root_Click(object sender, RoutedEventArgs e)
+        {
+            var val = Convert.ToDouble(panel.Content);
+            _val1 = Math.Sqrt(val);
+            panel.Content = _val1;
+        }
+
+        private void percent_Click(object sender, RoutedEventArgs e)
+        {
+            var val = Convert.ToDouble(panel.Content);
+            _val1 = val / 100;
+            panel.Content = _val1;
+        }
+
+        private void mult_Click(object sender, RoutedEventArgs e)
+        {
+            if (_sign != '*')
+            {
+                panel.Content = panel.Content.ToString() + " X ";
+                _sign = '*';
+            }
+            else
+            {
+                calculateEqual();
+            }
+        }
+        private void divide_Click(object sender, RoutedEventArgs e)
+        {
+            if (_sign != '/')
+            {
+                panel.Content = panel.Content.ToString() + " / ";
+                _sign = '/';
+            }
+            else
+            {
+                calculateEqual();
+            }
+        }
+        private void min_Click(object sender, RoutedEventArgs e)
+        {
+            if (_sign != '-')
+            {
+                panel.Content = panel.Content.ToString() + " - ";
+                _sign = '-';
+            }
+            else
+            {
+                calculateEqual();
+            }
+        }
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            if(_sign != '+')
+            {
+                panel.Content = panel.Content.ToString() + " + ";
+                _sign = '+';
+            }
+            else
+            {
+                calculateEqual();
+            }
+        }
+        private void calculateEqual()
+        {
+            var expressionArray = panel.Content.ToString().Split(' ');
+
+            //if (expressionArray.Length != 3)
+            //{
+            //    return;
+            //}
+
+            _val1 = Convert.ToDouble(expressionArray[0]);
+
+            if(expressionArray.Length == 3)
+            {
+                if(expressionArray[2] != "")
+                {
+                    _val2 = Convert.ToDouble(expressionArray[2]);
+                }
+            }
+
+            double? result = 0;
+
+            switch (_sign)
+            {
+                case '+':
+                    result = _val1 + _val2;
+                    break;
+                case '-':
+                    result = _val1 - _val2;
+                    break;
+                case '*':
+                    result = _val1 * _val2;
+                    break;
+                case '/':
+                    if (_val2 != 0)
+                    {
+                        result = _val1 / _val2;
+                    }
+                    break;
+            }
+
+            panel.Content = result;
+            _val1 = result;
+        }
+
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            calculateEqual();
         }
     }
 }
---
+++ Pipes/PipesProj/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace PipesProj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class ImageInfo
+        {
+            public Image Img { get; private set; } = new Image();
+            public double Angle { get; set; }
+
+
+            public ImageInfo()
+            {
+                var num = new Random().Next(1, 4);
+
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(@"\images\" + num.ToString() + ".png", UriKind.Relative);
+                bi.EndInit();
+
+                Img.Source = bi;
+                Img.Stretch = Stretch.Uniform;
+
+                Img.MouseLeftButtonDown += Img_MouseLeftButtonDown1;
+                Img.MouseRightButtonDown += Img_MouseRightButtonDown1;
+            }
+
+            private void Img_MouseRightButtonDown1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+            {
+                Image img = sender as Image;
+                img.RenderTransformOrigin = new Point(0.5, 0.5);
+                RotateTransform rotateTransform = new RotateTransform();
+                Angle -= 90;
+                rotateTransform.Angle = Angle;
+                img.RenderTransform = rotateTransform;
+            }
+
+            private void Img_MouseLeftButtonDown1(object sender, System.Windows.Input.MouseButtonEventArgs e)
+            {
+                Image img = sender as Image;
+                img.RenderTransformOrigin = new Point(0.5, 0.5);
+                RotateTransform rotateTransform = new RotateTransform();
+                Angle += 90;
+                rotateTransform.Angle = Angle;
+                img.RenderTransform = rotateTransform;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 16; i++)
+            {
+                ImageInfo imgInfo = new ImageInfo();
+                field.Children.Add(imgInfo.Img);
+            }
+        }
+    }
+}
