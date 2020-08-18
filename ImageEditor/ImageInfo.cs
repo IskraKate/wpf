d1850719a7de29895145022f3e7f7ec4f66ad026
Kate Iskra <kateiskra99@gmail.com>
2020-08-18 18:10:29 +0300

    cleanup code

--- ImageEditor/ImageInfo.cs
+++ ImageEditor/ImageInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
